dd5b0ec115afc09645fcabb7b28c3196b9372674
Sridhar <sridhar.ecet@gmail.com>
2024-08-17 19:01:29 +0530

    Second Commit

---
+++ Primitivevalues.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class PrimitiveValues
+{
+ static boolean B1;
+ static byte T1;
+ static int I1;
+ static float F1;
+ static double D1;
+ static char C1;
+ public static void main(String args[])
+ {
+  System.out.println("static  variable  Default values for BOOLEAN:" + B1);
+  System.out.println("static  variable  Default values for BYTE:" + T1);
+  System.out.println("static  variable  Default values for INT:" + I1);
+  System.out.println("static  variable  Default values for FLOAT:" + F1);
+  System.out.println("static  variable  Default values for DOUBLE:" + D1);
+  System.out.println("static  variable  Default values for CHAR:" + C1);
+  
+ }
+}
